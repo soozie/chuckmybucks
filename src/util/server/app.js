776371eb34f0a1c06d6d7a71a07880f63991004f
Nico Santoro <nicosantoro.26@gmail.com>
2018-09-25 18:42:37 +0100

    Created new endpoint to save and get users

--- src/util/server/app.js
+++ src/util/server/app.js
@@ -8,6 +8,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
 
+const INITIAL_DATA = {
+  expences: [],
+  users: []
+}
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -36,10 +41,7 @@ const server = app.listen(port, () => {
 
 const readFile = (err, data) => {
   if (err) {
-    const json = JSON.stringify({
-      expences: []
-    }); // initial state
-    fs.writeFile('data.json', json, 'utf8', (attrs) => console.log('!', attrs));
+    fs.writeFile('data.json', JSON.stringify(INITIAL_DATA), 'utf8', (attrs) => console.log('!', attrs));
     return false;
   } else {
     dataObj = JSON.parse(data);
@@ -55,6 +57,7 @@ const checkData = async () => {
 checkData();
 
 app.post('/api/saveExpence', function(req, res) {
+  console.log('Saving expence...');
   const userName = req.body.userName;
   const bucksAmount = req.body.bucksAmount;
   const when = req.body.when;
@@ -71,7 +74,7 @@ app.post('/api/saveExpence', function(req, res) {
       if (dataObj.expences) {
         const updatedExpences = [ ...dataObj.expences ];
         const id = _.uniqueId();
-        console.log(id);
+
         updatedExpences.push({
           userName,
           bucksAmount,
@@ -95,20 +98,94 @@ app.post('/api/saveExpence', function(req, res) {
   });
 });
 
+app.post('/api/saveUser', function(req, res) {
+  console.log('Saving user...');
+  const userName = req.body.userName;
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    let response;
+    if (err) {
+      response = { error: "We broke!" };
+    } else {
+      let newJsonData;
+      dataObj = JSON.parse(data);
+      if (dataObj.users) {
+        const updatedUsers = [ ...dataObj.users ];
+        const id = _.uniqueId();
+
+        updatedUsers.push({
+          userName
+        });
+        newJsonData = {
+          ...dataObj,
+          users: updatedUsers
+        };
+        fs.writeFile('data.json', JSON.stringify(newJsonData), 'utf8', (attrs) => {});
+        response = updatedUsers;
+      } else {
+        response = { error: "We broke!" };
+      }
+    }
+
+    res.json(response);
+  });
+});
+
 app.get('/api/getExpences', async function(req, res) {
-  console.log('Gotcha!');
+  console.log('Getting expences...');
   let response;
+
   fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) {
-      const json = JSON.stringify({
-        expences: []
-      }); // initial state
-      fs.writeFile('data.json', json, 'utf8', (attrs) => console.log('!', attrs));
       response = { error: "We broke!" };
     } else {
       dataObj = JSON.parse(data);
       response = dataObj.expences;
     }
     res.json(response);
+  });
+});
+
+app.get('/api/resetExpences', async function(req, res) {
+  console.log('Resetting expences...');
+
+  fs.writeFile('data.json', JSON.stringify({
+    ...INITIAL_DATA,
+    expences: []
+  }), 'utf8', (attrs) => {
+    res.json({
+      ok: true,
+      ...INITIAL_DATA
+    });
+  });
+});
+
+app.get('/api/getUsers', async function(req, res) {
+  console.log('Getting users...');
+  let response;
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      response = { error: "We broke!" };
+    } else {
+      dataObj = JSON.parse(data);
+      response = dataObj.users;
+    }
+    res.json(response);
+  });
+});
+
+
+app.get('/api/resetUsers', async function(req, res) {
+  console.log('Resetting users...');
+
+  fs.writeFile('data.json', JSON.stringify({
+    ...INITIAL_DATA,
+    users: []
+  }), 'utf8', (attrs) => {
+    res.json({
+      ok: true,
+      ...INITIAL_DATA
+    });
   });
 });
